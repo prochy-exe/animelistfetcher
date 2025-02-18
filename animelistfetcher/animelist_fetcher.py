494c914b568b4e615653ebe50f94f2bce2e0e4bb
Dominik Procházka <dominikprochazka100@gmail.com>
2025-02-18 09:57:37 +0100

    amount and format support

--- animelistfetcher/animelist_fetcher.py
+++ animelistfetcher/animelist_fetcher.py
@@ -26,7 +26,7 @@ def config():
     print("Starting MyAnimeList setup")
     malfetcher.config_setup()
 
-def get_latest_anime_entry_for_user(service, status = "ALL", service_token = None, username = None):
+def get_latest_anime_entry_for_user(service, status = "ALL", media_format = None, service_token = None, username = None):
     service = service.lower()
     if service == "al" or service == "anilist":
         import alfetcher
@@ -35,14 +35,14 @@ def get_latest_anime_entry_for_user(service, status = "ALL", service_token = Non
         import malfetcher
         return malfetcher.get_latest_anime_entry_for_user(status, service_token, username)
 
-def get_all_anime_for_user(service, status_array = "ALL", service_token = None, username = None):
+def get_all_anime_for_user(service, status_array = "ALL", media_format = None, amount = 0, service_token = None, username = None):
     service = service.lower()
     if service == "al" or service == "anilist":
         import alfetcher
-        return alfetcher.get_all_anime_for_user(status_array, service_token, username)
+        return alfetcher.get_all_anime_for_user(status_array, media_format, amount, service_token, username)
     elif service == "mal" or service == "myanimelist":
         import malfetcher
-        return malfetcher.get_all_anime_for_user(status_array, service_token, username)
+        return malfetcher.get_all_anime_for_user(status_array, media_format, amount, service_token, username)
 
 def get_anime_entry_for_user(service, anime_id, service_token = None, username = None):
     service = service.lower()
@@ -62,14 +62,14 @@ def get_anime_info(service, anime_id, service_token = None):
         import malfetcher
         return malfetcher.get_anime_info(anime_id, False, service_token)
 
-def get_id(service, search_string, service_token = None):
+def get_id(service, search_string, media_format = None, amount = 1, service_token = None):
     service = service.lower()
     if service == "al" or service == "anilist":
         import alfetcher
-        return alfetcher.get_id(search_string, service_token)
+        return alfetcher.get_id(search_string, media_format, amount, service_token)
     elif service == "mal" or service == "myanimelist":
         import malfetcher
-        return malfetcher.get_id(search_string, service_token)
+        return malfetcher.get_id(search_string, media_format, amount, service_token)
 
 def update_entry(service, anime_id, progress, service_token=None):
     service = service.lower()
